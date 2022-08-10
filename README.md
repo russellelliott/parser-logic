85ce620ad9fb4703ee2da5c03a107bb121848cc8
Russell Elliott <russell@quadsquad.net>
2022-08-10 10:44:51 -0700

    able to get education level from resume

--- README.md
+++ README.md
@@ -27,11 +27,25 @@ name: "Billy Bob"
 In a terminal window, type `node parser.js` to run the program.
 
 ## Resources
-Getting user input: https://www.w3schools.com/jsref/met_win_prompt.asp <br>
-Convert file to string: <br>
+### Getting user input:
+https://www.w3schools.com/jsref/met_win_prompt.asp
+
+### Convert file to string:
 https://stackoverflow.com/questions/34857458/reading-local-text-file-into-a-javascript-array <br>
 https://www.geeksforgeeks.org/javascript-program-to-read-text-file/ <br>
-Get first N words of a string: https://bobbyhadz.com/blog/javascript-get-first-n-words-of-string <br>
-Split string by newline and space: <br>
+
+### Get first N words of a string:
+https://bobbyhadz.com/blog/javascript-get-first-n-words-of-string
+
+### Split string:
+https://www.w3schools.com/jsref/jsref_split.asp
+### Split string by newline and space:
 https://stackoverflow.com/questions/9849754/how-can-i-replace-newlines-line-breaks-with-spaces-in-javascript <br>
 https://stackoverflow.com/questions/17271324/split-string-by-space-and-new-line-in-javascript <br>
+
+### Iterate through string:
+https://tutorial.eyehunts.com/js/loop-through-words-in-a-string-javascript-iterate-words-in-a-string-in-js/ <br>
+https://stackoverflow.com/questions/2430000/determine-if-string-is-in-list-in-javascript <br>
+
+### Convert array to string:
+https://www.w3schools.com/jsref/jsref_tostring_array.asp
--- parser.js
+++ parser.js
@@ -11,15 +11,32 @@ function readFile(fileName){
 
 }
 
+function getWords(sourceFile, resumeFile){
+    var readSourceFile = readFile(sourceFile);
+    const sourceFileWords = readSourceFile.split(" "); //split source file by spaces
+
+    var readResumeFile = readFile(resumeFile);
+
+    var wordsList = [];
+    for(var i=0; i<sourceFileWords.length; i++){ //iterate through all words of source file
+        var currentWord = sourceFileWords[i];
+        //check if the resume file includes the current word. if so, add it to the list if not already.
+        if(readResumeFile.includes(currentWord) && !wordsList.includes(currentWord)){
+            wordsList.push(currentWord);
+        }
+    }
+    //convert words list into string
+    wordsList = wordsList.toString(); //toString() method returns a string with array values separated by commas.
+    //ex: ["apples", "oranges", "bananas"] -> "apples,oranges,bananas"
+    return wordsList;
+}
 //Step 2: Parse the file
 function getEducation(fileName){
     var educationFile = "education.txt"
-    var educationNames = readFile(educationFile); //get list of education types
-
+    var education = getWords(educationFile, fileName);
+    return education;
     
 }
-var education = "" //education
-var languages = "" //languages
 
 function getName(fileName){
     //First 2 words
@@ -31,6 +48,9 @@ function getName(fileName){
     return name
 }
 
+var education = getEducation(fileName) //education
+console.log(education);
+var languages = "" //languages
 const name = getName(fileName);
 console.log(name);
 
